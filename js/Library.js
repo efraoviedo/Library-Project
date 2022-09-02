fa08bd8bacb6979dcbf1d49de0346bc46708c055
efranor <efraoviedo@gmail.com>
2022-09-02 09:43:33 -0500

    se trabajo el forulario con api

--- js/Library.js
+++ js/Library.js
@@ -6,7 +6,7 @@ function Book() {
 
 function addBookToLibrary() {
   // do stuff here
-}let myLibrary = [];
+}let NewLibrary = [];
 
 function Book() {
   // the constructor...
@@ -15,3 +15,42 @@ function Book() {
 function addBookToLibrary() {
   // do stuff here
 }
+
+
+const form = document.querySelector("form");
+const email = document.getElementById("mail");
+const emailError = document.querySelector("#mail + span.error");
+
+email.addEventListener("input", (event) => {
+
+  if (email.validity.valid) {
+
+    emailError.textContent = ""; 
+    emailError.className = "error"; 
+  } else {
+
+    showError();
+  }
+});
+
+form.addEventListener("submit", (event) => {
+  if (!email.validity.valid) {
+    showError();
+    event.preventDefault();
+  }
+});
+
+function showError() {
+  if (email.validity.valueMissing) {
+
+    emailError.textContent = "You need to enter an e-mail address.";
+  } else if (email.validity.typeMismatch) {
+    emailError.textContent = "Entered value needs to be an e-mail address.";
+  } else if (email.validity.tooShort) {
+    emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
+  }
+
+  emailError.className = "error active";
+}
+
+
